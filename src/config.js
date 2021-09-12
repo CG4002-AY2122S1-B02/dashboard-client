84f4f3f002faafad2a534e40af32dc8802748a8b
tzw0 <e0310410@u.nus.edu>
2021-09-13 05:27:42 +0800

    3 websocket

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const DashboardServerPath = "localhost:8081/apis/"
+export const User1Port = 8881
+export const User2Port = 8882
+export const User3Port = 8883
+
+export const PathUser1Stream = "ws://" + DashboardServerPath + "stream/ws/" + User1Port
+export const PathUser2Stream = "ws://" + DashboardServerPath + "stream/ws/" + User2Port
+export const PathUser3Stream = "ws://" + DashboardServerPath + "stream/ws/" + User3Port
